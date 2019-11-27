dcd44afc2ddf2d395472846f183f1b3d75bbc6ac
RehimliTF <togrul.rehimli@kapitalbank.az>
2019-11-27 11:56:27 +0400

    simple changes

--- src/main/java/repository/DatabaseInfo.java
+++ src/main/java/repository/DatabaseInfo.java
@@ -2,18 +2,16 @@ package repository;
 
 import model.User;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DatabaseInfo {
 
-    private List<User> users = new ArrayList<>();
-
-    {
-        users.add(new User(1, "Togrul", "togrul", "togrul123"));
-        users.add(new User(2, "Taleh", "taleh", "taleh123"));
-        users.add(new User(3, "Farhad", "farhad", "farhad123"));
-    }
+    private List<User> users = Arrays.asList(
+            new User(1, "Togrul", "togrul", "togrul123"),
+            new User(2, "Taleh", "taleh", "taleh123"),
+            new User(3, "Farhad", "farhad", "farhad123")
+    );
 
 
     public User authUser(String username, String password) {
